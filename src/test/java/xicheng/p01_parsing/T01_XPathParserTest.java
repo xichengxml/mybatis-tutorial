770458a47eb80fe0411b8beaf47b0dee00d335e1
xichengxml <xichengxml@163.com>
2020-12-12 23:42:47 +0800

    测试类

--- src/test/java/xicheng/p01_parsing/T01_XPathParserTest.java
+++ src/test/java/xicheng/p01_parsing/T01_XPathParserTest.java
@@ -2,13 +2,10 @@ package xicheng.p01_parsing;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.parsing.XPathParser;
 import org.junit.Test;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.IOException;
 
 /**
  * description
@@ -30,7 +27,7 @@ public class T01_XPathParserTest {
 
     @Test
     public void constructorTest() throws Exception {
-        // 这种方式是错误的，需要把xml转换成String，通过eval获取
+        // 这种方式是错误的，需要把xml转换成String，通过eval获取+
         XPathParser xPathParser = new XPathParser(RESOURCE);
         log.info("T01_XPathParserTest constructorTest xPathParser: {}", JSON.toJSONString(xPathParser));
     }
