402e0e1425d6f210f9807c8c5d4853e1b12a496a
xichengxml <xichengxml@163.com>
2020-12-12 23:41:39 +0800

    测试类

--- src/test/java/xicheng/p01_parsing/T01_XPathParserTest.java
+++ src/test/java/xicheng/p01_parsing/T01_XPathParserTest.java
@@ -1,9 +1,14 @@
 package xicheng.p01_parsing;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.parsing.XPathParser;
 import org.junit.Test;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * description
@@ -14,10 +19,20 @@ import javax.xml.parsers.DocumentBuilderFactory;
 @Slf4j
 public class T01_XPathParserTest {
 
+    private static final String RESOURCE = "resources/nodelet_test.xml";
+
     @Test
     public void documentBuilderFactoryTest() {
         DocumentBuilderFactory instance01 = DocumentBuilderFactory.newInstance();
         DocumentBuilderFactory instance02 = DocumentBuilderFactory.newInstance();
         log.info("T01_XPathParserTest documentBuilderFactoryTest result: {}", instance01 == instance02);
     }
+
+    @Test
+    public void constructorTest() throws Exception {
+        // 这种方式是错误的，需要把xml转换成String，通过eval获取
+        XPathParser xPathParser = new XPathParser(RESOURCE);
+        log.info("T01_XPathParserTest constructorTest xPathParser: {}", JSON.toJSONString(xPathParser));
+    }
+
 }
