037ead15133ebd7e5086786de07ff032e3b0c5d0
xichengxml <xichengxml@163.com>
2020-12-27 23:15:26 +0800

    反射模块注释

--- src/main/java/org/apache/ibatis/reflection/invoker/MethodInvoker.java
+++ src/main/java/org/apache/ibatis/reflection/invoker/MethodInvoker.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.invoker;
 
@@ -21,39 +21,48 @@ import java.lang.reflect.Method;
 import org.apache.ibatis.reflection.Reflector;
 
 /**
+ * class类中对应get或者set方法的封装
  * @author Clinton Begin
  */
 public class MethodInvoker implements Invoker {
 
-  private final Class<?> type;
-  private final Method method;
+    /**
+     * 类型
+     */
+    private final Class<?> type;
 
-  public MethodInvoker(Method method) {
-    this.method = method;
+    /**
+     * 指定方法
+     */
+    private final Method method;
 
-    if (method.getParameterTypes().length == 1) {
-      type = method.getParameterTypes()[0];
-    } else {
-      type = method.getReturnType();
+    public MethodInvoker(Method method) {
+        this.method = method;
+        if (method.getParameterTypes().length == 1) {
+            // 参数长度为1时，一般是set方法，将type设置为参数0的类型
+            type = method.getParameterTypes()[0];
+        } else {
+            // 否则，一般是get方法，将type设置为方法的返回值类型
+            type = method.getReturnType();
+        }
     }
-  }
 
-  @Override
-  public Object invoke(Object target, Object[] args) throws IllegalAccessException, InvocationTargetException {
-    try {
-      return method.invoke(target, args);
-    } catch (IllegalAccessException e) {
-      if (Reflector.canControlMemberAccessible()) {
-        method.setAccessible(true);
-        return method.invoke(target, args);
-      } else {
-        throw e;
-      }
+    @Override
+    public Object invoke(Object target, Object[] args) throws IllegalAccessException, InvocationTargetException {
+        try {
+            return method.invoke(target, args);
+        } catch (IllegalAccessException e) {
+            if (Reflector.canControlMemberAccessible()) {
+                method.setAccessible(true);
+                return method.invoke(target, args);
+            } else {
+                throw e;
+            }
+        }
     }
-  }
 
-  @Override
-  public Class<?> getType() {
-    return type;
-  }
+    @Override
+    public Class<?> getType() {
+        return type;
+    }
 }
